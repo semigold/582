73e960953f3a5c1eb6a8b41dcc9239818a7ca0a0
Clyde Byrd III <clydebyrdiii@yahoo.com>
2016-03-25 21:32:26 -0400

    Fixed port initialization

--- cpu/msp430/fr5xxx/msp430.c
+++ cpu/msp430/fr5xxx/msp430.c
@@ -84,65 +84,42 @@ init_ports(void)
 {
   /* Turn everything off, device drivers enable what is needed. */
   /* All configured for digital I/O */
-#ifdef P1SEL
-  P1SEL = 0;
-#endif
-#ifdef P2SEL
-  P2SEL = 0;
-#endif
-#ifdef P3SEL
-  P3SEL = 0;
-#endif
-#ifdef P4SEL
-  P4SEL = 0;
-#endif
-#ifdef P5SEL
-  P5SEL = 0;
-#endif
-#ifdef P6SEL
-  P6SEL = 0;
-#endif
+  
+  P1SEL0 = 0;
+  P1SEL1 = 0;
+  
+  P2SEL0 = 0;
+  P2SEL1 = 0;
+
+  P3SEL0 = 0;
+  P3SEL1 = 0;
+
+  P4SEL0 = 0;
+  P4SEL1 = 0;
+
+  PJSEL0 = 0;
+  PJSEL1 = 0;
 
   /* All available inputs */
-#ifdef P1DIR
   P1DIR = 0;
   P1OUT = 0;
-#endif
-#ifdef P2DIR
+
   P2DIR = 0;
   P2OUT = 0;
-#endif
-#ifdef P3DIR
+
   P3DIR = 0;
   P3OUT = 0;
-#endif
-#ifdef P4DIR
+
   P4DIR = 0;
   P4OUT = 0;
-#endif
 
-#ifdef P5DIR
-  P5DIR = 0;
-  P5OUT = 0;
-#endif
-
-#ifdef P6DIR
-  P6DIR = 0;
-  P6OUT = 0;
-#endif
-
-#ifdef P7DIR
-  P7DIR = 0;
-  P7OUT = 0;
-#endif
-
-#ifdef P8DIR
-  P8DIR = 0;
-  P8OUT = 0;
-#endif
+  PJDIR = 0;
+  PJOUT = 0;
 
   P1IE = 0;
   P2IE = 0;
+  P3IE = 0;
+  P4IE = 0;
 }
 /*---------------------------------------------------------------------------*/
 /* msp430-ld may align _end incorrectly. Workaround in cpu_init. */
